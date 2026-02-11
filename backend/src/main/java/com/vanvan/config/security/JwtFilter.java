cb1e07e637352bcb367c5bd5ad087f0ef983d9fa
cauaof_souza <cauasantos064@gmail.com>
2026-02-11 20:35:24 -0300

    Cria JwtFilter e SecurityConfig

---
+++ backend/src/main/java/com/vanvan/config/security/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.vanvan.config.security;
+
+import com.vanvan.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component//bean para usar na config
+@RequiredArgsConstructor //injeta dependências da classe pelo construtor (lombok)
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if (token != null){
+            var email = jwtService.validateAndGetSubject(token);
+            UserDetails user = userRepository.findByEmail(email);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null) return null;
+
+        return authHeader.replace("Bearer ", "");
+    }
+}
---
+++ backend/src/main/java/com/vanvan/config/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.vanvan.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //essa config deixa abertos login e register endpoints [por ora],
+    // dentre alguns das tools de desenvolvimento, de resto, somente com autenticação
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) {
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/auth/login",
+                                "/api/auth/register")
+                        .permitAll()
+                        .requestMatchers(//para swagger
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()//caso venha ser usada essa tool
+                        .anyRequest().authenticated()
+                ).headers(headers -> headers.frameOptions(frame -> frame.disable()))//para o console
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
+    }
+
+    //beans para auth
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
