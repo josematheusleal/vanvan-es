61f6c9c1a85741e68fe23a90ea702159a03a16af
Matheus Leal <matheus010307@hotmail.com>
2026-03-05 20:24:46 -0300

    fix: tabela de clientes

--- frontend/src/app/pages/clients/clients-edit/clients-edit.ts
+++ frontend/src/app/pages/clients/clients-edit/clients-edit.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ClienteService, Cliente } from '../../../services/client.service'; 
 
 @Component({
   selector: 'app-cliente-edit',
@@ -8,37 +9,42 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, FormsModule],
   templateUrl: './clients-edit.html',
 })
-export class ClienteEditComponent implements OnInit {
+export class ClienteEditComponent {
 
-  @Input() cliente: any; // Recebe o cliente clicado na tabela
+  @Input() set cliente(value: Partial<Cliente> | null) {
+    if (value) {
+      this.clienteEditado = { ...value };
+    }
+  }
+
   @Output() aoFechar = new EventEmitter<boolean>();
 
   clienteEditado: any = {}; // Cópia local para edição
   mostrarSenha = false;
   carregando = false;
 
-  constructor() {}
-
-  ngOnInit(): void {
-    if (this.cliente) {
-      this.clienteEditado = { ...this.cliente };
-    }
-  }
+  constructor(private clienteService: ClienteService) {}
 
   fechar() {
-    this.aoFechar.emit(false); // Emite 'false' indicando que apenas fechou/cancelou
+    this.aoFechar.emit(false);
   }
 
   salvar() {
+    // CORRIGIDO: Estava usando this.motoristaEditado
     if (!this.clienteEditado.id) return;
-
+    
     this.carregando = true;
-    console.log('Editando cliente:', this.clienteEditado);
 
-    // SIMULAÇÃO (Substitua pelo serviço real)
-    setTimeout(() => {
-      this.carregando = false;
-      this.aoFechar.emit(true); // Emite 'true' indicando que salvou com sucesso
-    }, 1000);
+    // CORRIGIDO: Chamando o clienteService e passando apenas o clienteEditado
+    this.clienteService.editar(this.clienteEditado).subscribe({
+      next: () => {
+        this.carregando = false;
+        this.aoFechar.emit(true);
+      },
+      error: (err: any) => {
+        console.error('Erro:', err);
+        this.carregando = false;
+      }
+    });
   }
 }
