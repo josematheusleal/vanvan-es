f799d50e84bb8135c49d1e4db8e832dc5152ab26
P3DROVFX <pedrolucasmunizpessoa@gmail.com>
2026-03-06 21:47:39 -0300

    Criando animações globais com o angular animations

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,14 +2,20 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './sidebar/sidebar';
 import { Toast } from './components/toast/toast';
+import { routeAnimations } from './app.animations';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Toast],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  animations: [routeAnimations]
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  getRouteAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.isActivated ? outlet.activatedRoute?.component : null;
+  }
 }
 
--- frontend/src/app/layout/main-layout.ts
+++ frontend/src/app/layout/main-layout.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
+import { routeAnimations } from '../app.animations';
 
 @Component({
   selector: 'app-main-layout',
   imports: [RouterOutlet, Sidebar],
   templateUrl: './main-layout.html',
+  animations: [routeAnimations]
 })
-export class MainLayout {}
+export class MainLayout {
+  getRouteAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.isActivated ? outlet.activatedRoute?.component : null;
+  }
+}
